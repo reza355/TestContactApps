ef12950072d0717887435c20815b71acaab1257e
Fathureza Januarza <fathureza.januarza@tokopedia.com>
2020-09-15 21:38:26 +0700

    Initial Commit

---
+++ ContactApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactApps
+//
+//  Created by Fathureza Januarza on 15/09/20.
+//  Copyright © 2020 Fathureza Januarza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
